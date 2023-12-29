d1cc1d421bb602e65207b8d74f023edf1f1630a3
JoseDev <joseluisdp297@gmail.com>
2023-12-29 11:03:11 +0000

    Add login funcionality

--- app.py
+++ app.py
@@ -60,6 +60,28 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # comprobar is existe el usuario
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash(f"Welcome, {request.form.get('username')}")
+            else:
+                # paswword incorrecto
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        else:
+            # User no existe
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+    return render_template("login.html")
+
+
 # Si se ejecuta este archivo como script principal
 if __name__ == "__main__":
     # Ejecuta la aplicación Flask con la configuración de las variables de entorno
