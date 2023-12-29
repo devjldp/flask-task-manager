c2eb3bd4d35c8aad92aa36de796d893e11e9a2ed
JoseDev <joseluisdp297@gmail.com>
2023-12-29 12:07:39 +0000

    Bind data from db to acordeon

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 # Define la ruta "/register" para el registro de nuevos usuarios
